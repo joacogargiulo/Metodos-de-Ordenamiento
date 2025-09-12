d5d31ba352b780091344df63d4da422f1b1653fa
Joaquin Gargiulo <136387324+joacogargiulo@users.noreply.github.com>
2025-09-12 17:36:33 -0300

    Revert "commit de inicio"
    This reverts commit 804eba29589999e7aa664382947a29bb11b37767.

--- src/main/java/com/joaquin/metodosordenamiento/MetodosOrdenamiento.java
+++ src/main/java/com/joaquin/metodosordenamiento/MetodosOrdenamiento.java
@@ -5,11 +5,40 @@ import java.util.Scanner;
 public class MetodosOrdenamiento {
 
     public static void main(String[] args) {
-        InterfazGrafica GUI = new InterfazGrafica();
-        GUI.setVisible(true);
-        GUI.setLocationRelativeTo(null);
+        Scanner teclado = new Scanner(System.in);
+        int cantidadElementos;
+        int contadorOE = 0;
+        int burbujaMenorMayorT;        
+        
+        System.out.println("Ingrese la cantidad de elementos a ordenar:");
+        cantidadElementos = teclado.nextInt();
+        Vector vector = new Vector(cantidadElementos);
+        Vector menorMayor = new Vector(cantidadElementos);
+        Vector insercionBinaria = new Vector(cantidadElementos);
+        
+        if (cantidadElementos > 10) {
+            vector.cargaRandom(0, cantidadElementos*10);
+        } else {
+            vector.cargarVector("Ingrese un numero entero:");
+        }
         
         
         
+        burbujaMenorMayorT = vector.clonar().ordenamientoBurbujaMenorAMayor();
+            
+        System.out.println("\nOrdenamient Burbuja de menor a mayor: T(" + burbujaMenorMayorT + ")\n");
+        
+        
+        
+        insercionBinaria = vector.clonar();
+        int insercionBinariaT = insercionBinaria.ordenamientoInsecionBinaria();
+        
+        insercionBinaria.imprimirVector();
+       
+        //System.out.println("\nOrdenamient Burbuja de menor a mayor: T(" + insercionBinariaT + ")\n");
+        
+        
+        
+
     }
 }
